486f902e70c19393ed0b62058646c6bed2e5c2da
Niels Perfors <niro1987@users.noreply.github.com>
2023-06-28 11:52:23 +0200

    Fix-typehints (#7)
    ## Changes
    
    - All non str, int, date, datetime, bool and Value fields are now expanded by default.
    - Renamed _encode _decode to improve readability
    - Add helper function _meta to generate field metadata
    - Updated pylint config to exlude tests

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,9 +1,12 @@
+"""Tests for the sapcommissions.resources module."""
+from __future__ import annotations
+
 import unittest
 from dataclasses import dataclass, field
 from datetime import date, datetime
-from typing import ClassVar, List
+from typing import ClassVar
 
-from sapcommissions.resources import _decode, _encode, _Resource
+from sapcommissions.resources import _deserialize, _Resource, _serialize
 
 
 @dataclass
@@ -11,8 +14,8 @@ class DummyResource(_Resource):
     _endpoint_name: ClassVar[str] = "test"
     id: str = field(metadata={"id": True})
     name: str = field(default=None)
-    items: List[str] = field(default_factory=list)
-    expands: bool = field(default=None, metadata={"expand": True})
+    items: list[str] = field(default_factory=list)
+    expands: dict = field(default=None)
 
 
 class TestResourceMethods(unittest.TestCase):
@@ -21,7 +24,6 @@ class TestResourceMethods(unittest.TestCase):
             "id": "123",
             "name": "Test Resource",
             "items": ["item1", "item2"],
-            "expands": False,
         }
         self.resource: DummyResource = DummyResource.from_dict(self.resource_dict)
 
@@ -41,21 +43,23 @@ class TestResourceMethods(unittest.TestCase):
         self.assertEqual(self.resource._id, "123")
 
     def test_expands_property(self):
-        self.assertEqual(DummyResource._expands, ("expands",))
+        self.assertEqual(
+            DummyResource._expands,
+            ("items", "expands"),
+        )
 
     def test_from_dict_method(self):
         self.assertIsInstance(self.resource, DummyResource)
         self.assertEqual(self.resource.id, "123")
         self.assertEqual(self.resource.name, "Test Resource")
         self.assertEqual(self.resource.items, ["item1", "item2"])
-        self.assertEqual(self.resource.expands, False)
+        self.assertEqual(self.resource.expands, None)
 
     def test_to_dict_method(self):
         expected_dict = {
             "id": "123",
             "name": "Test Resource",
             "items": ["item1", "item2"],
-            "expands": False,
         }
         self.assertEqual(self.resource.to_dict(), expected_dict)
 
@@ -63,75 +67,75 @@ class TestResourceMethods(unittest.TestCase):
 class TestEncodeDecode(unittest.TestCase):
     def test_decode_datetime(self):
         dt_str = "2000-01-02T03:04:05.000-06:00"
-        decoded_dt = _decode(dt_str, datetime)
+        decoded_dt = _deserialize(dt_str, datetime)
         self.assertEqual(decoded_dt, datetime.fromisoformat(dt_str))
 
     def test_encode_datetime(self):
         dt = datetime(2000, 1, 2, 3, 4, 5)
-        encoded_dt = _encode(dt, datetime)
+        encoded_dt = _serialize(dt, datetime)
         self.assertEqual(encoded_dt, "2000-01-02T03:04:05")
 
     def test_decode_date(self):
         date_str = "2000-01-02T03:04:05.000-06:00"
-        decoded_date = _decode(date_str, date)
+        decoded_date = _deserialize(date_str, date)
         self.assertEqual(decoded_date, datetime.fromisoformat(date_str).date())
 
     def test_encode_date(self):
         dt = date(2000, 1, 2)
-        encoded_dt = _encode(dt, date)
+        encoded_dt = _serialize(dt, date)
         self.assertEqual(encoded_dt, "2000-01-02")
 
     def test_decode_none(self):
-        decoded_none = _decode(None, str)
+        decoded_none = _deserialize(None, str)
         self.assertIs(decoded_none, None)
 
     def test_encode_none(self):
-        encoded_none = _encode(None, str)
+        encoded_none = _serialize(None, str)
         self.assertIs(encoded_none, None)
 
     def test_decode_string(self):
         test_str = "SPAM"
-        decoded_str = _decode(test_str, str)
+        decoded_str = _deserialize(test_str, str)
         self.assertEqual(decoded_str, "SPAM")
 
     def test_encode_string(self):
         test_str = "SPAM"
-        encoded_str = _encode(test_str, str)
+        encoded_str = _serialize(test_str, str)
         self.assertEqual(encoded_str, "SPAM")
 
     def test_decode_int(self):
         test_int = 1
-        decoded_int = _decode(test_int, int)
+        decoded_int = _deserialize(test_int, int)
         self.assertEqual(decoded_int, 1)
 
     def test_decode_str_int(self):
         test_int_str = "1"
-        decoded_int_str = _decode(test_int_str, int)
+        decoded_int_str = _deserialize(test_int_str, int)
         self.assertEqual(decoded_int_str, 1)
 
     def test_encode_int(self):
         test_int = 1
-        encoded_int = _encode(test_int, int)
+        encoded_int = _serialize(test_int, int)
         self.assertEqual(encoded_int, 1)
 
     def test_decode_list_str(self):
         test_list = ["SPAM", "EGGS"]
-        decoded_list = _decode(test_list, list[str])
+        decoded_list = _deserialize(test_list, list[str])
         self.assertListEqual(decoded_list, test_list)
 
     def test_decode_list_error(self):
         test_list = ["SPAM", "EGGS"]
         with self.assertRaises(TypeError):
-            _decode(test_list, list[str, int])
+            _deserialize(test_list, list[str, int])
         with self.assertRaises(TypeError):
-            _decode(test_list, list)
+            _deserialize(test_list, list)
         with self.assertRaises(TypeError):
-            _decode(test_list, str)
+            _deserialize(test_list, str)
 
     def test_decode_unions(self):
         test_value = 1
         with self.assertRaises(NotImplementedError):
-            _decode(test_value, int | dict)
+            _deserialize(test_value, int | dict)
 
 
 if __name__ == "__main__":
