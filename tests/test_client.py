19df731e5c399a99fc0c5993afc8d093bf85262c
Niels Perfors <niels.perfors1987@gmail.com>
2024-09-30 21:24:26 +0000

    update exceptions
    - add SAPNotFound
    - clean up some others
    - add message to SAPBadRequest

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,1 +1,21 @@
 """Test for SAP Commissions Client."""
+# pylint: disable=all
+
+from datetime import datetime
+
+from sapcommissions import CommissionsClient, model
+
+
+async def test_client_create_participant(
+    client: CommissionsClient,
+) -> None:
+    """Test the CommissionsClient."""
+    pa: model.Participant = model.Participant(
+        user_id="DUMMY",
+        payee_id="DUMMY",
+        last_name="DUMMY",
+        effective_start_date=datetime(2024, 1, 1),
+        effective_end_date=datetime(2200, 1, 1),
+    )
+    result = await client.create(pa)
+    print(result)  # noqa: T201
