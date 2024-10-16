914abb74a1c3e00861ff2205113bfa17821141f1
Niels Perfors <niels.perfors1987@gmail.com>
2024-10-16 19:39:21 +0000

    update tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -32,8 +32,41 @@ async def fixture_delete_pipeline(
         try:
             await client.cancel_pipeline(pipeline)
             LOGGER.info("Pipeline cancelled: %s", pipeline.pipeline_run_seq)
-        except Exception as exc:  # pylint: disable=broad-except
-            LOGGER.error("Error deleting pipeline: %s", exc)
+        except Exception:  # pylint: disable=broad-except
+            LOGGER.exception("Error deleting pipeline")
+
+
+@pytest.fixture(name="calendar", scope="session")
+async def fixture_calendar(client: CommissionsClient) -> model.Calendar:
+    """Fixture to return first calendar instance."""
+    if not (calendar := await client.read_first(model.Calendar)):
+        pytest.skip("No calendar returned from client.")
+    assert calendar.seq is not None, "calendar.seq invalid."
+
+    LOGGER.debug("Calendar %s: %s", calendar.seq, calendar.name)
+    return calendar
+
+
+@pytest.fixture(name="period", scope="session")
+async def fixture_period(
+    client: CommissionsClient,
+    calendar: model.Calendar,
+) -> model.Period:
+    """Fixture to return first calendar period instance."""
+    if not (
+        period := await client.read_first(
+            model.Period,
+            filters=helpers.And(
+                helpers.Equals("calendar", str(calendar.seq)),
+                helpers.Equals("periodType", str(calendar.minor_period_type)),
+            ),
+        ),
+    ):
+        pytest.skip("No period returned from client.")
+    assert period.seq, "period.seq invalid."
+
+    LOGGER.debug("Period %s: %s", period.seq, period.name)
+    return period
 
 
 @pytest.mark.parametrize(
@@ -62,15 +95,10 @@ async def test_pipelinerun(
     client: CommissionsClient,
     pipeline_job: type[T],
     cleanup: list[model.Pipeline],
+    period: model.Period,
 ) -> None:
     """Test running a pipeline on a calendar period."""
-    period: model.Period | None = await client.read_first(
-        model.Period,
-        filters=helpers.Equals("name", "202401 W1"),
-    )
-    if not period:
-        pytest.skip("No period returned from client.")
-    assert period.period_seq is not None, "period_seq not found."
+
     job: T = pipeline_job(  # type: ignore[call-arg]
         calendar_seq=str(period.calendar),
         period_seq=period.period_seq,
@@ -81,37 +109,7 @@ async def test_pipelinerun(
     cleanup.append(result)
     assert result.command == job.command
     assert result.stage_type == job.stage_type_seq
-    assert result.period == period.period_seq
-
-
-async def test_pipelinerun_report(
-    client: CommissionsClient,
-    cleanup: list[model.Pipeline],
-) -> None:
-    """Test running a pipeline on a calendar period."""
-    period: model.Period | None = await client.read_first(
-        model.Period,
-        filters=helpers.Equals("name", "202401 W1"),
-    )
-    if not period:
-        pytest.skip("No period returned from client.")
-
-    assert period.period_seq is not None, "period_seq not found."
-    job: model.ReportsGeneration = model.ReportsGeneration(
-        calendar_seq=str(period.calendar),
-        period_seq=period.period_seq,
-        report_type_name=const.ReportType.Crystal,
-        report_formats_list=[const.ReportFormat.Excel],
-        ods_report_list=["Outbound Files"],
-        bo_groups_list=["VFNL Compensation Reports Admin Group"],
-    )
-    result: model.Pipeline = await client.run_pipeline(job)
-    LOGGER.info(result)
-    assert result.pipeline_run_seq is not None
-    cleanup.append(result)
-    assert result.command == job.command
-    assert result.stage_type == job.stage_type_seq
-    assert result.period == period.period_seq
+    assert str(result.period) == period.seq
 
 
 async def test_xmlimport(
@@ -146,19 +144,12 @@ async def test_import(
     client: CommissionsClient,
     pipeline_job: type[model.pipeline._ImportJob],
     cleanup: list[model.Pipeline],
+    calendar: model.Calendar,
 ) -> None:
     """Test running an import job."""
     batch_name: str = "test.txt"
-    calendar: model.Calendar | None = await client.read_first(
-        model.Calendar,
-        filters=helpers.Equals("name", "Main Weekly Calendar"),
-    )
-    if not calendar:
-        pytest.skip("No calendar returned from client.")
-
-    assert calendar.calendar_seq is not None, "calendar_seq not found."
     job: model.pipeline._ImportJob = pipeline_job(  # type: ignore[call-arg]
-        calendar_seq=calendar.calendar_seq,
+        calendar_seq=calendar.seq,
         batch_name=batch_name,
         module=const.StageTables.TransactionalData,
     )
@@ -193,20 +184,13 @@ async def test_purge(
 async def test_resetfromvalidate(
     client: CommissionsClient,
     cleanup: list[model.Pipeline],
+    period: model.Period,
 ) -> None:
     """Test running a ResetFromValidate pipeline."""
     batch_name: str = "test.txt"
-    period: model.Period | None = await client.read_first(
-        model.Period,
-        filters=helpers.Equals("name", "202001 W1"),
-    )
-    if not period:
-        pytest.skip("No period returned from client.")
-
-    assert period.period_seq is not None, "period_seq not found."
     job: model.ResetFromValidate = model.ResetFromValidate(
         calendar_seq=str(period.calendar),
-        period_seq=period.period_seq,
+        period_seq=period.seq,
         batch_name=batch_name,
     )
     result: model.Pipeline = await client.run_pipeline(job)
@@ -221,19 +205,12 @@ async def test_resetfromvalidate(
 async def test_resetfromvalidate_no_batch(
     client: CommissionsClient,
     cleanup: list[model.Pipeline],
+    period: model.Period,
 ) -> None:
     """Test running a ResetFromValidate pipeline without batch_name."""
-    period: model.Period | None = await client.read_first(
-        model.Period,
-        filters=helpers.Equals("name", "202001 W1"),
-    )
-    if not period:
-        pytest.skip("No period returned from client.")
-
-    assert period.period_seq is not None, "period_seq not found."
     job: model.ResetFromValidate = model.ResetFromValidate(
         calendar_seq=str(period.calendar),
-        period_seq=period.period_seq,
+        period_seq=period.seq,
     )
     result: model.Pipeline = await client.run_pipeline(job)
     LOGGER.info(result)
